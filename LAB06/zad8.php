34724f3f4c44f59bf3e9504d9992a609160b6d0b
oktaflat <155980698+oktaflat@users.noreply.github.com>
2024-04-26 00:37:26 +0200

    Update zad8.php

--- LAB06/zad8.php
+++ LAB06/zad8.php
@@ -22,7 +22,7 @@
     <div>
         <img src="https://t4.ftcdn.net/jpg/05/52/82/11/360_F_552821106_RVVWBchtNowOCZIEZHqx04BGzz1Jz0KG.jpg" alt="kapibara">
 
-        <php? if(KapibaraJeMarchewke()) : ?>
+        <?php if(KapibaraJeMarchewke()) : ?>
             <img src="https://png.pngtree.com/png-vector/20190305/ourmid/pngtree-fresh-carrot-image-png-image_727953.jpg" alt=marchew>
         <?php endif; ?>
     </div>
